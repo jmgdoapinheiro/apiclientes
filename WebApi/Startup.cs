496f6bbcd8b52188784b8445651b9565c9764992
João Marcelo Gurgel do Amaral Pinheiro <joao.pinheiro@wooza.com.br>
2020-09-13 14:32:24 -0300

    adicao de swagger.

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Service;
 
 namespace WebApi
@@ -22,17 +23,28 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
             services.AddTransient<IClienteService, ClienteService>();
             services.AddTransient<IClienteRepository, ClienteRepository>();
             services.AddTransient<IEnderecoService, EnderecoService>();
             services.AddTransient<IEnderecoRepository, EnderecoRepository>();
 
-            services.AddControllers();
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "API Clientes" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Clientes");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
